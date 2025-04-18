fa9009edd4088416c900bc38d8418ca3de2c1bc1
AMIT KUMAR <101931012+AMIT2110990161@users.noreply.github.com>
2025-04-18 14:06:21 +0530

    Update NameAsParameter.java

--- messageApp/src/main/java/com/RAGA/messageApp/useCases/NameAsParameter.java
+++ messageApp/src/main/java/com/RAGA/messageApp/useCases/NameAsParameter.java
@@ -1,16 +1,1 @@
-package com.RAGA.messageApp.useCases;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/user")
-public class NameAsParameter {
-    @GetMapping("/value")
-    public String getName(@RequestParam String name){
-        return "Hello " + name + " Bridgelabz mai apka shugat hai";
-    }
-}
